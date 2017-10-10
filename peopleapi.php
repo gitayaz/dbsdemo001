5458ec243b774bf78b8e9e6a21c7f70c13cdfa87
gitayaz <32671064+gitayaz@users.noreply.github.com>
2017-10-10 13:35:20 +0100

    Update peopleapi.php

--- peopleapi.php
+++ peopleapi.php
@@ -10,11 +10,11 @@ class Person {
 
 $items = array();
 for($i=0; $i<10; $i++) {
-$personRecord = new Person();
-$personRecord->personID = $i;
-$personRecord->firstName = "Name $i";
-$personRecord->lastName = "LastName $i";
-$items[$i] = $personRecord;
+    $personRecord = new Person();
+    $personRecord->personID = $i;
+    $personRecord->firstName = "Name $i";
+    $personRecord->lastName = "LastName $i";
+    $items[$i] = $personRecord;
 }
 header('Content-Type: application/json');
 $jsonOutput = json_encode($items);
