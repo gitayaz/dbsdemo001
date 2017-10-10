490bb82086b0cd8a549a89a950720b2761248ffc
gitayaz <32671064+gitayaz@users.noreply.github.com>
2017-10-10 12:58:28 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
-echo "<h1>Hello World from Heroku</h1>";
+echo "<h1><b>Hello World from Heroku VERSION 2</b></h1>";
 echo "<p> Hello World from Heroku</p>";
 echo "<p>This is a new paragraph</p>";
